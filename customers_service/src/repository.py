c3c2fdf9d8ea29ecae736173e476ef1d2a15f438
Amir Asaad <amir.pro.2010@gmail.com>
2023-08-08 17:12:45 +0300

    refactor: :recycle: PureSQLRepository replace db_curosr by db_connection in constractor, ensure atmoic transactions

--- customers_service/src/repository.py
+++ customers_service/src/repository.py
@@ -8,13 +8,14 @@ logger = logging.getLogger(__name__)
 
 
 class PureSQLRepository:
-    def __init__(self, db_cursor, db_table_name: str = "Customers"):
-        self.db_cursor = db_cursor
+    def __init__(self, db_connection, db_table_name: str = "Customers"):
+        self.db_connection = db_connection
         self.db_table_name = db_table_name
 
     def save(self, customer: Customer):
+        cursor = self.db_connection.cursor()
         try:
-            self.db_cursor.execute(
+            cursor.execute(
                 """INSERT INTO {}(first_name,last_name, email) VALUES ({},{},{})""".format(
                     self.db_table_name,
                     customer.first_name,
@@ -25,10 +26,12 @@ class PureSQLRepository:
         except Exception as e:
             logger.critical(str(e))
             raise DatabaseError(str(e))
+        self.db_connection.commit()
 
     def get(self, customer_id: int):
+        cursor = self.db_connection.cursor()
         try:
-            self.db_cursor.execute(
+            cursor.execute(
                 """SELECT first_name,last_name,email from {} WHERE id = {}""".format(
                     self.db_table_name,
                     customer_id,
@@ -37,11 +40,12 @@ class PureSQLRepository:
         except Exception as e:
             logger.critical(str(e))
             raise DatabaseError(str(e))
-        return Customer.from_dict(self.db_cursor.fetchone())
+        return Customer.from_dict(cursor.fetchone())
 
     def update(self, customer_id: int, customer_dict: dict):
+        cursor = self.db_connection.cursor()
         try:
-            self.db_cursor.execute(
+            cursor.execute(
                 """UPDATE {} SET first_name = {} SET last_name = {}, SET email = {}  WHERE id = {}""".format(
                     self.db_table_name,
                     customer_dict["first_name"],
@@ -53,10 +57,12 @@ class PureSQLRepository:
         except Exception as e:
             logger.critical(str(e))
             raise DatabaseError(str(e))
+        self.db_connection.commit()
 
     def delete(self, customer_id: int):
+        cursor = self.db_connection.cursor()
         try:
-            self.db_cursor.execute(
+            cursor.execute(
                 """DELETE first_name,last_name,email from {} WHERE id = {}""".format(
                     self.db_table_name, customer_id
                 )
@@ -64,3 +70,4 @@ class PureSQLRepository:
         except Exception as e:
             logger.critical(str(e))
             raise DatabaseError(str(e))
+        self.db_connection.commit()
--- customers_service/tests/unit/test_repository.py
+++ customers_service/tests/unit/test_repository.py
@@ -1,4 +1,4 @@
-from unittest.mock import Mock
+from unittest.mock import MagicMock, Mock
 
 import pytest
 from ...src.exceptions import DatabaseError
@@ -8,19 +8,21 @@ from ...src.repository import PureSQLRepository
 
 
 def repo_with_mocked_cursor():
-    db_mock_cursor = Mock()
-    repository = PureSQLRepository(db_mock_cursor)
-    return db_mock_cursor, repository
+    db_mock_connection = MagicMock()
+    repository = PureSQLRepository(db_mock_connection)
+    mock_cursor = MagicMock()
+    db_mock_connection.cursor.return_value = mock_cursor
+    return repository, mock_cursor, db_mock_connection
 
 
 class TestPureSQLRepository:
     def test_save_customer(self):
-        db_mock_cursor, repository = repo_with_mocked_cursor()
+        repository, mock_cursor, db_mock_connection= repo_with_mocked_cursor()
         customer = Customer(
             first_name="John", last_name="Smith", email="john@example.com"
         )
         repository.save(customer)
-        db_mock_cursor.execute.assert_called_once_with(
+        mock_cursor.execute.assert_called_once_with(
             """INSERT INTO {}(first_name,last_name, email) VALUES ({},{},{})""".format(
                 repository.db_table_name,
                 customer.first_name,
@@ -28,10 +30,11 @@ class TestPureSQLRepository:
                 customer.email,
             )
         )
+        db_mock_connection.commit.assert_called_once()
 
     def test_save_handle_db_exception_and_log_error(self, caplog):
-        db_mock_cursor, repository = repo_with_mocked_cursor()
-        db_mock_cursor.execute.side_effect = Mock(side_effect=Exception("Test"))
+        repository, mock_cursor, _ = repo_with_mocked_cursor()
+        mock_cursor.execute.side_effect = Exception("Test")
         customer = Customer(
             first_name="John", last_name="Smith", email="john@example.com"
         )
@@ -42,44 +45,41 @@ class TestPureSQLRepository:
         assert "Test" in caplog.text
 
     def test_repository_get_customer_by_id(self):
-        db_mock_cursor, repository = repo_with_mocked_cursor()
+        repository, mock_cursor, _ = repo_with_mocked_cursor()
         customer_dict = {
             "first_name": "John",
             "last_name": "Smith",
             "email": "john@example.com",
         }
-        db_mock_cursor.fetchone.return_value = customer_dict
+        mock_cursor.fetchone.return_value = customer_dict
         db_customer = repository.get(customer_id=1)
-        db_mock_cursor.execute.assert_called_once_with(
+        mock_cursor.execute.assert_called_once_with(
             """SELECT first_name,last_name,email from Customers WHERE id = 1"""
         )
-        db_mock_cursor.fetchone.assert_called_once()
+        mock_cursor.fetchone.assert_called_once()
         assert Customer.from_dict(customer_dict) == db_customer
 
     def test_repository_delete_customer_by_id(self):
-        db_mock_cursor, repository = repo_with_mocked_cursor()
-        customer_dict = {
-            "first_name": "John",
-            "last_name": "Smith",
-            "email": "john@example.com",
-        }
+        repository, mock_cursor, db_mock_connection = repo_with_mocked_cursor()
         repository.delete(customer_id=1)
-        db_mock_cursor.execute.assert_called_once_with(
+        mock_cursor.execute.assert_called_once_with(
             """DELETE first_name,last_name,email from Customers WHERE id = 1"""
         )
+        db_mock_connection.commit.assert_called_once()
 
     def test_repository_update_customer_by_id(self):
-        db_mock_cursor, repository = repo_with_mocked_cursor()
+        repository, mock_cursor, db_mock_connection = repo_with_mocked_cursor()
         customer_dict = {
             "first_name": "John",
             "last_name": "Smith",
             "email": "john@example.com",
         }
         repository.update(1, customer_dict)
-        db_mock_cursor.execute.assert_called_once_with(
+        mock_cursor.execute.assert_called_once_with(
             """UPDATE Customers SET first_name = {} SET last_name = {}, SET email = {}  WHERE id = 1""".format(
                 customer_dict["first_name"],
                 customer_dict["last_name"],
                 customer_dict["email"],
             )
         )
+        db_mock_connection.commit.assert_called_once()
